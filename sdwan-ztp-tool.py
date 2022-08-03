23d55c4acdabf2e4b3da8ffebd6ca511a25fa6fc
Baris <s.barissonmez@gmail.com>
2022-08-03 23:07:01 +0300

    Update sdwan-ztp-tool.py

--- sdwan-ztp-tool.py
+++ sdwan-ztp-tool.py
@@ -18,13 +18,10 @@ requests.packages.urllib3.disable_warnings()
 #   *Default allowed_extensions are: ['.js', '.html', '.txt', '.htm', '.xhtml']
 eel.init('web', allowed_extensions=['.js', '.html'])
 
-
 def sendupdate(return_html):
     eel.pageupdate(return_html)
 
 ### Export ADOM Functions
-
-
 def export_adom(adomname):
     global export_info
     global sdwan_template_list
@@ -49,3 +46,16 @@ def export_adom(adomname):
     res = session.post(fmgurl, json=jsondata, verify=False)
     parsed = json.loads(res.text)
     # print(json.dumps(parsed, indent=4, sort_keys=True))
+
+    dvmdbadom = parsed['result'][0]['data']
+    dvmdbadom.pop('uuid', None)
+    dvmdbadom.pop('oid', None)
+    dvmdbadom["name"] = "$(adom_name)"
+    dvmdbadom["desc"] = "$(adom_desc)"
+    if adomname == "root":
+        dvmdbadom["flags"] = 2312
+
+    newdict["data"].append(dvmdbadom)
+    export_info["settings"].append(newdict)
+
+    ## standard objects (objects which can be exported and imported with out changing anything, mutliple objects can be created in a list)
