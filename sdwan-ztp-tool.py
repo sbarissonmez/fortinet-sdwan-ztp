cbd08a553551f517c2c51c10108bb88867d8169d
Baris <s.barissonmez@gmail.com>
2022-08-03 23:05:25 +0300

    Update sdwan-ztp-tool.py

--- sdwan-ztp-tool.py
+++ sdwan-ztp-tool.py
@@ -17,3 +17,35 @@ requests.packages.urllib3.disable_warnings()
 # Set web files folder and optionally specify which file types to check for eel.expose()
 #   *Default allowed_extensions are: ['.js', '.html', '.txt', '.htm', '.xhtml']
 eel.init('web', allowed_extensions=['.js', '.html'])
+
+
+def sendupdate(return_html):
+    eel.pageupdate(return_html)
+
+### Export ADOM Functions
+
+
+def export_adom(adomname):
+    global export_info
+    global sdwan_template_list
+    global polpkg_list
+    export_info = {"vars": ["adom_name", "adom_desc"], "settings": []}
+    requestid = 1
+
+    ## Get ADOM Info
+    newdict = {"url": "/dvmdb/adom/", "method": "add", "data": []}
+
+    jsondata = {
+        "method": "get",
+        "params": [
+            {
+                "url": "/dvmdb/adom/" + adomname
+            }
+
+        ],
+        "id": requestid,
+        "session": fmg_sessionid
+    }
+    res = session.post(fmgurl, json=jsondata, verify=False)
+    parsed = json.loads(res.text)
+    # print(json.dumps(parsed, indent=4, sort_keys=True))
