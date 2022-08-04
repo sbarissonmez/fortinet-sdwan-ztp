09a37bc513357e94e8429f9db8922585af1f2ed9
Baris <s.barissonmez@gmail.com>
2022-08-04 14:11:03 +0300

    Update sdwan-ztp-tool.py

--- sdwan-ztp-tool.py
+++ sdwan-ztp-tool.py
@@ -57,3 +57,82 @@ def export_adom(adomname):
     export_info["settings"].append(newdict)
 
     ## standard objects (objects which can be exported and imported with out changing anything, mutliple objects can be created in a list)
+
+    sdwan_template_list = []
+    polpkg_list = []
+
+    std_objects = {
+        "clitemplate": ["/pm/config/adom/$(adom_name)/obj/cli/template",
+                        "/pm/config/adom/" + adomname + "/obj/cli/template", ["modification-time"]],
+        "clitemplate-group": ["/pm/config/adom/$(adom_name)/obj/cli/template-group",
+                              "/pm/config/adom/" + adomname + "/obj/cli/template-group", ["modification-time"]],
+        "sdwaninterface": ["/pm/config/adom/$(adom_name)/obj/dynamic/virtual-wan-link/members/",
+                           "/pm/config/adom/" + adomname + "/obj/dynamic/virtual-wan-link/members/",
+                           ["dynamic_mapping", "obj seq"]],
+        "sdwanservers": ["/pm/config/adom/$(adom_name)/obj/dynamic/virtual-wan-link/server/",
+                         "/pm/config/adom/" + adomname + "/obj/dynamic/virtual-wan-link/server/",
+                         ["dynamic_mapping"]],
+        "sdwantemplates": ["/pm/wanprof/adom/$(adom_name)",
+                           "/pm/wanprof/adom/" + adomname,
+                           ["scope member", "oid"]],
+        "addrobjs": ["pm/config/adom/$(adom_name)/obj/firewall/address/",
+                     "pm/config/adom/" + adomname + "/obj/firewall/address/",
+                     ["uuid", "dynamic_mapping"]],
+        "addrobjs_grp": ["pm/config/adom/$(adom_name)/obj/firewall/addrgrp/",
+                         "pm/config/adom/" + adomname + "/obj/firewall/addrgrp/",
+                         ["uuid", "dynamic_mapping"]],
+        "intfobjs": ["pm/config/adom/$(adom_name)/obj/dynamic/interface/",
+                     "pm/config/adom/" + adomname + "/obj/dynamic/interface/",
+                     ["uuid", "dynamic_mapping"]],
+        "applist": ["pm/config/adom/$(adom_name)/obj/application/list",
+                    "pm/config/adom/" + adomname + "/obj/application/list",
+                    ["uuid", "dynamic_mapping", "obj seq"]],
+        "appgrp": ["pm/config/adom/$(adom_name)/obj/application/group",
+                   "pm/config/adom/" + adomname + "/obj/application/group",
+                   ["uuid", "dynamic_mapping", "obj seq"]],
+        "service": ["pm/config/adom/$(adom_name)/obj/firewall/service/custom",
+                    "pm/config/adom/" + adomname + "/obj/firewall/service/custom",
+                    ["uuid", "dynamic_mapping", "obj seq"]],
+        "servicegrp": ["pm/config/adom/$(adom_name)/obj/firewall/service/group",
+                       "pm/config/adom/" + adomname + "/obj/firewall/service/group",
+                       ["uuid", "dynamic_mapping", "obj seq"]],
+        "polpkg": ["pm/pkg/adom/$(adom_name)",
+                   "pm/pkg/adom/" + adomname,
+                   ["scope member", "oid"]]
+
+    }
+
+    for objecturls in std_objects:
+        get_and_add(std_objects, objecturls)
+
+    ## Get SDWAN Template Details
+    for sdwan_template in sdwan_template_list:
+        get_and_add({"sdwan_member": [
+            "pm/config/adom/$(adom_name)/wanprof/" +
+            sdwan_template + "/system/virtual-wan-link/member",
+            "pm/config/adom/" + adomname + "/wanprof/" +
+            sdwan_template + "/system/virtual-wan-link/member",
+            ["obj seq"]]}, "sdwan_member")
+
+        get_and_add({"sdwan_hlth": [
+            "pm/config/adom/$(adom_name)/wanprof/" + sdwan_template +
+            "/system/virtual-wan-link/health-check",
+            "pm/config/adom/" + adomname + "/wanprof/" +
+            sdwan_template + "/system/virtual-wan-link/health-check",
+            ["obj seq"]]}, "sdwan_hlth")
+
+        get_and_add({"sdwan_service": [
+            "pm/config/adom/$(adom_name)/wanprof/" +
+            sdwan_template + "/system/virtual-wan-link/service",
+            "pm/config/adom/" + adomname + "/wanprof/" +
+            sdwan_template + "/system/virtual-wan-link/service",
+            ["obj seq"]]}, "sdwan_service")
+
+    ## Get Policy Package Details
+    for polpkg in polpkg_list:
+        get_and_add({"polpkg_policy": ["pm/config/adom/$(adom_name)/pkg/" + polpkg + "/firewall/policy",
+                                       "pm/config/adom/" + adomname + "/pkg/" + polpkg + "/firewall/policy",
+                                       ["obj seq", "_policy_block"]]}, "polpkg_policy")
+
+    # print(json.dumps(export_info, indent=4, sort_keys=True))
+    return json.dumps(export_info, indent=4, sort_keys=True)
